b7519b55e9d3a1634ec98a4fd678870284972baa
TA <tuyetanh82198@gmail.com>
2024-10-04 17:47:52 +0700

    update

--- app.js
+++ app.js
@@ -27,13 +27,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  const allowedOrigins = process.env.CLIENT;
-  const origin = req.headers.origin;
-  if (allowedOrigins === origin) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
   next();
 });
 app.use(
