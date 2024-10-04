e25ada702804734b3419cb2445fb25b460bcd02e
TA <tuyetanh82198@gmail.com>
2024-10-04 17:41:13 +0700

    update

--- app.js
+++ app.js
@@ -26,6 +26,14 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  const allowedOrigins = process.env.CLIENT;
+  const origin = req.headers.origin;
+  res.setHeader("Access-Control-Allow-Origin", origin);
+  // res.setHeader("Access-Control-Allow-Methods", "POST, GET");
+  // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
